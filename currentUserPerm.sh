db9bb8a461ea1c3352a25841e9b52c3825a19e75
headmin <henry@macadmin.me>
2017-07-05 18:42:52 +0200

    process symlinks

--- currentUserPerm.sh
+++ currentUserPerm.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# Run this script via Outset or in Self-Service. 
+# Run this script via Outset or in Self-Service.
 # in case you'll run manually you need to use sudo.
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 
+    echo "This script must be run as root"
     exit 1
 fi
 
-# Get current logged in user
 loggedInUser=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
 
-# Add staff to _developer group
+# Add staff to developer group
 dseditgroup -o edit -a staff -t group _developer
 
-# chgrp scope to _developer 
-groupScope="_developer" 
+# use _developer group
+groupScope="_developer"
 
 /bin/chmod u+rwx /usr/local/bin
 /bin/chmod g+rwx /usr/local/bin
-/usr/sbin/chown -R $loggedInUser /usr/local/bin
-/usr/bin/chgrp -R $groupScope /usr/local/bin
-/bin/mkdir -p /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var
-/bin/chmod g+rwx /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var
+# process  symlinks only, ensure to exclude symlinks for jamf, autopkg, outset, santactl
+find /usr/local/bin/ -type l -and ! -name "jamf" -and ! -name "autopkg" -and ! -name "outset" ! -name "santactl"  -exec chown -R $loggedI$
+find /usr/local/bin/ -type l -and ! -name "jamf" -and ! -name "autopkg" -and ! -name "outset" ! -name "santactl"  -exec chgrp -R $groupSc$
+/bin/mkdir -p /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt$
+/bin/chmod g+rwx /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/etc /usr/local/include /usr/local/lib /usr/local/$
 /bin/chmod 755 /usr/local/share/zsh /usr/local/share/zsh/site-functions
-/usr/sbin/chown -R $loggedInUser /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var
-/usr/bin/chgrp -R $groupScope /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var
+/usr/sbin/chown -R $loggedInUser /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/etc /usr/local/include /usr/local$
+/usr/bin/chgrp -R $groupScope /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/etc /usr/local/include /usr/local/li$
 /bin/mkdir -p /Users/$loggedInUser/Library/Caches/Homebrew
 /bin/chmod g+rwx /Users/$loggedInUser/Library/Caches/Homebrew
 /usr/sbin/chown -R $loggedInUser /Users/$loggedInUser/Library/Caches/Homebrew
